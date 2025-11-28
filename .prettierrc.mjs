a04c60eed00f27b171c9c17f04a5d3c4cf1adf66
lazakrisz <lezer11@icloud.com>
2025-11-28 18:42:08 +0100

    chore: setting up prettier and initial tsdown

---
+++ .prettierrc.mjs
@@ -0,0 +1,40 @@
+/**
+ * @type {import('prettier').Options}
+ */
+export default {
+  printWidth: 80,
+  tabWidth: 2,
+  useTabs: false,
+  semi: true,
+  jsxSingleQuote: false,
+  singleQuote: false,
+  arrowParens: "always",
+  trailingComma: "all",
+  bracketSpacing: true,
+  bracketSameLine: false,
+  tailwindFunctions: ["cn"],
+  customFunctions: [
+    // Nicer
+    "cn",
+    "cva",
+  ],
+  attributeGroups: [
+    "^key$",
+    "^class$",
+    "^(id|name)$",
+    "$DEFAULT",
+    "^data-",
+    "^aria-",
+  ],
+  attributeSort: "ASC",
+  plugins: [
+    "prettier-plugin-sort-json",
+    "prettier-plugin-packagejson",
+    "@xeonlink/prettier-plugin-organize-attributes",
+    "prettier-plugin-multiline-arrays",
+    "prettier-plugin-organize-imports",
+    "prettier-plugin-classnames",
+    "prettier-plugin-brace-style",
+    "prettier-plugin-tailwindcss",
+  ],
+};
