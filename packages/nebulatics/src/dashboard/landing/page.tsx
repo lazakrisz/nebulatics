a889fdd3ac1afb4c1b83de4dfd6ca9c2b214298c
lazakrisz <lezer11@icloud.com>
2025-11-29 08:56:59 +0100

    it works

--- packages/nebulatics/src/dashboard/landing/page.tsx
+++ packages/nebulatics/src/dashboard/landing/page.tsx
@@ -2,6 +2,8 @@ import { headers } from "next/headers";
 
 export async function LandingPage() {
   const headersStore = await headers();
+  const headersArray = Array.from(headersStore.entries());
+  console.log("headersArray", headersArray);
 
   return <div>LandingPage</div>;
 }
